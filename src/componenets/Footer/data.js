35af3ef60cf691f322c53fb8016753386f0469ba
Shivang Agarwal <shivangagarwal11.sa@gmail.com>
2022-12-12 22:27:58 +0530

    mycard-ui

---
+++ src/componenets/Footer/data.js
@@ -0,0 +1,67 @@
+export const datajson = {
+  data: [
+    {
+      title: "About",
+      links: [
+        {
+          label: "Features",
+          link: "#"
+        },
+        {
+          label: "Pricing",
+          link: "#"
+        },
+        {
+          label: "Support",
+          link: "#"
+        },
+        {
+          label: "Forums",
+          link: "#"
+        }
+      ]
+    },
+    {
+      title: "Updates",
+      links: [
+        {
+          label: "Query",
+          link: "#"
+        },
+        {
+          label: "Media assets",
+          link: "#"
+        },
+        {
+          label: "Changelog",
+          link: "#"
+        },
+        {
+          label: "Releases",
+          link: "#"
+        }
+      ]
+    },
+    {
+      title: "Community",
+      links: [
+        {
+          label: "Join MyCard Community",
+          link: "#"
+        },
+        {
+          label: "Follow on Twitter",
+          link: "#"
+        },
+        {
+          label: "Email newsletter",
+          link: "#"
+        },
+        {
+          label: "Discussions",
+          link: "#"
+        }
+      ]
+    }
+  ]
+};
